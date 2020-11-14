2918d2c354bda33ff83c7a3d1fc3fe7af37158b1
ermanimer <ermanimer@gmail.com>
2020-11-14 09:47:40 +0300

    Remove caller function from log message

--- log_viewer.go
+++ log_viewer.go
@@ -33,18 +33,16 @@ const (
 )
 
 type Log struct {
-	datetime       string
-	prefix         string
-	callerFunction string
-	message        string
+	datetime string
+	prefix   string
+	message  string
 }
 
-var longestCallerFunctionLength int
 var longestPrefixLength int
 
 func main() {
 	filename := flag.String("f", filename, "filename")
-	prefixes := flag.String("p", prefixes, "prefixes to view.")
+	prefixes := flag.String("p", prefixes, "prefixes to view")
 	flag.Parse()
 	viewLogs(*filename, *prefixes)
 }
@@ -65,7 +63,6 @@ func viewLogs(filename string, prefixes string) {
 	//read lines from log file
 	var logs []Log
 	longestPrefixLength = 0
-	longestCallerFunctionLength = 0
 	reader := bufio.NewReader(logFile)
 	for {
 		bytesOfLine, _, err := reader.ReadLine()
@@ -98,7 +95,6 @@ func viewLogs(filename string, prefixes string) {
 		}
 		if viewLog {
 			logs = append(logs, *log)
-			updateLongestCallerFunctionLength(log.callerFunction)
 			updateLongestPrefixLength(log.prefix)
 		}
 	}
@@ -110,23 +106,15 @@ func viewLogs(filename string, prefixes string) {
 
 func parseLog(line string) (*Log, error) {
 	segments := strings.Split(line[1:len(line)-1], "][")
-	if len(segments) != 4 {
+	if len(segments) != 3 {
 		return nil, errors.New("parsing log failed")
 	}
 	log := Log{
-		datetime:       segments[0],
-		prefix:         segments[1],
-		callerFunction: segments[2],
-		message:        segments[3],
+		datetime: segments[0],
+		prefix:   segments[1],
+		message:  segments[2],
 	}
 	return &log, nil
-}
-
-func updateLongestCallerFunctionLength(callerFunction string) {
-	callerFunctionLength := len(callerFunction)
-	if callerFunctionLength > longestCallerFunctionLength {
-		longestCallerFunctionLength = callerFunctionLength
-	}
 }
 
 func updateLongestPrefixLength(prefix string) {
@@ -138,20 +126,13 @@ func updateLongestPrefixLength(prefix string) {
 
 func viewLog(log *Log) {
 	setPrefixLength(log)
-	setCallerFunctionLength(log)
 	setPrefixColor(log)
-	setCallerFunctionColor(log)
-	fmt.Printf("[%v][%v][%v]%v%v", log.datetime, log.prefix, log.callerFunction, log.message, newLine)
+	fmt.Printf("[%s][%s]%s%s", log.datetime, log.prefix, log.message, newLine)
 }
 
 func setPrefixLength(log *Log) {
 	padding := strings.Repeat(" ", longestPrefixLength-len(log.prefix))
-	log.prefix = fmt.Sprintf("%v%v", log.prefix, padding)
-}
-
-func setCallerFunctionLength(log *Log) {
-	padding := strings.Repeat(" ", longestCallerFunctionLength-len(log.callerFunction))
-	log.callerFunction = fmt.Sprintf("%v%v", log.callerFunction, padding)
+	log.prefix = fmt.Sprintf("%s%s", log.prefix, padding)
 }
 
 func setPrefixColor(log *Log) {
@@ -167,8 +148,4 @@ func setPrefixColor(log *Log) {
 	case fatalPrefix:
 		log.prefix = color.Red(log.prefix)
 	}
-}
-
-func setCallerFunctionColor(log *Log) {
-	log.callerFunction = color.Magenta(log.callerFunction)
 }
